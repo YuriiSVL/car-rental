b1d327f7c69b7ab94e2069b1a2063e6fb5446d4b
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-09-21 01:19:29 +0300

    delete comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-// import logo from "./logo.svg";
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import { lazy } from 'react';
-// import CarList from "./components/CarList/CarList";
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('./pages/AllCarsPage/AllCarsPage'));
 const Favorites = lazy(() =>
@@ -20,25 +18,6 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
-
-    // <CarList />
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,61 @@
+import css from './HomePage.module.css';
+
 const MainPage = () => {
   return (
-    <section>
-      <h1>Hello, this is home page</h1>
+    <section className={css.container}>
+      <h1>Introducing Ukraine's Premier Car Rental Service!</h1>
+      <p>
+        Discover the beauty of Ukraine at your own pace with our top-notch car
+        rental service. Whether you're exploring the historic streets of Kyiv,
+        admiring the natural wonders of the Carpathian Mountains, or taking a
+        road trip along the scenic Black Sea coast, we've got the perfect
+        vehicle for your adventure.
+      </p>
+      <h2>Why Choose Us?</h2>
+      <ul className={css.list}>
+        <li>
+          <p>
+            Wide Range of Vehicles: From compact cars to spacious SUVs, we have
+            a diverse fleet to suit your needs and preferences.
+          </p>
+        </li>
+        <li>
+          <p>
+            Affordable Rates: Enjoy competitive rental rates and flexible
+            pricing options. We offer the best value for your money.
+          </p>
+        </li>
+        <li>
+          <p>
+            Convenient Booking: Our user-friendly online platform allows you to
+            book your rental in minutes. No hassle, no fuss.
+          </p>
+        </li>
+        <li>
+          <p>
+            24/7 Support: Our dedicated customer support team is available
+            around the clock to assist you with any questions or concerns.
+          </p>
+        </li>
+        <li>
+          <p>
+            Safety First: Your safety is our priority. Our vehicles are
+            regularly serviced and maintained to ensure a worry-free journey.
+          </p>
+        </li>
+        <li>
+          <p>
+            Explore Ukraine: Unlock the freedom to explore Ukraine's hidden
+            gems, from charming villages to vibrant cities, at your own pace.
+          </p>
+        </li>
+      </ul>
+      <p>
+        Make your Ukrainian travel experience unforgettable with our reliable
+        and efficient car rental service. Book your vehicle today and embark on
+        an incredible journey through the heart of Ukraine.
+      </p>
+      <p>Discover Ukraine. Drive with us!</p>
     </section>
   );
 };
